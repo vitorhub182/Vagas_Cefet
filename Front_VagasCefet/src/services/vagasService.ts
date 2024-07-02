5752f09476a8a292b148a9d566c74049ef5a2284
João Vitor <106121350+vitorhub182@users.noreply.github.com>
2024-07-02 00:23:49 -0300

    Ajustando telas de vagas

--- Front_VagasCefet/src/services/vagasService.ts
+++ Front_VagasCefet/src/services/vagasService.ts
@@ -1,7 +1,10 @@
-import { ListaVagasDTO } from "@/dto/vagas";
+import { DescricaoVagasDTO, ListaVagasDTO } from "@/dto/vagas";
 
 export async function listaVagas() {
   const token = sessionStorage.getItem('access_token');
+  if (!token){
+    throw new Error('Token não encontrado!')
+  }
   try{
     const response = await fetch('http://localhost:3002/vagas/', {
       method: 'GET',
@@ -28,3 +31,35 @@ export async function listaVagas() {
     throw new Error('Falha ao se conectar com a api');
   }
   }
+
+  export async function descricaoVaga(id : string) {
+    const token = sessionStorage.getItem('access_token');
+    if (!token){
+      throw new Error('Token não encontrado!')
+    }
+    try{
+      const response = await fetch(`http://localhost:3002/vagas/${id}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        },
+      });
+    
+      if (response.status == 401 ) {
+        
+        const dados: any  = [];
+        return dados; 
+  
+      }else if (response.status == 200){
+        const dados: DescricaoVagasDTO = await response.json();
+        console.log(dados);
+        return dados;
+      }else {
+        throw new Error('Falha ao registrar usuario');
+      }
+    } catch (error){
+      console.log(error);
+      throw new Error('Falha ao se conectar com a api');
+    }
+    }
